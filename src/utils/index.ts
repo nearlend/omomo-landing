b1123a2f01855ff07c62996020a93b97cda9537c
AlexanderOsadchyy <alex.o@blaize.tech>
2022-06-02 14:33:47 +0300

    Update seo desc

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
 // Basic app settings
 export const APP_SEO_TITLE: string = 'Omomo';
-export const APP_SEO_DESCRIPTION: string = 'Seo description ...';
+export const APP_SEO_DESCRIPTION: string = '';
 export const APP_DOMAIN_URL: string = 'https://omomo.finance/';

1751ab57b2a96b5751386f5c827360648dcbae8f
dmytroShev <dmytro.s@blaze.tech>
2022-11-11 14:09:25 +0200

    Update about section

--- src/home/sections/About/index.tsx
+++ src/home/sections/About/index.tsx
@@ -5,53 +5,38 @@ const About = () => {
   return (
     <section className={s.about}>
       <div className="container">
-        <h2 className={s.about__title}>About us</h2>
+        <h2 className={s.about__title}>What is OMOMO finance</h2>
         <div className={s.about__row}>
           <div className={s.about__block}>
-            <img src={'/assets/images/icons/reward.svg'} alt="Reward" />
-            <div className={s.about__block__info}>
-              <h4>Near.Hack winner</h4>
-              <p>
-                The Near.Hack-winning solution is bringing non-custodial borrowing and lending to
-                the NEAR MainNet. Are you ready for liquidity protocol on the most user-friendly
-                blockchain?
-              </p>
+            <div className={s.about__block__product}>
+              <img src={'/assets/images/icons/omomo.svg'} alt="omomo" />
+              ОМОМО <br /> finance
+            </div>
+
+            <img
+              src={'/assets/images/triple-arrow.svg'}
+              className={s.about__block__arrow}
+              alt="arrow"
+            />
+            <div className={s.about__block__features}>
+              <div className={s.about__block__features__item}>
+                <img src={'/assets/images/icons/drop-left.svg'} alt="omomo" />
+                Lending/Borrowing Protocol
+              </div>
+              <div className={s.about__block__features__item}>
+                <img src={'/assets/images/icons/circle.svg'} alt="omomo" />
+                Decentralised Leverage trading
+              </div>
+              <div className={s.about__block__features__item}>
+                <img src={'/assets/images/icons/circle.svg'} alt="omomo" />
+                Limit orders on DEX
+              </div>
+              <div className={s.about__block__features__item}>
+                <img src={'/assets/images/icons/drop-right.svg'} alt="omomo" />
+                Derivatives and yield farming strategies
+              </div>
             </div>
           </div>
-
-          {/*<div className={s.about__block}>*/}
-          {/*  <img src={About1} alt="Reward"/>*/}
-          {/*  <div className={s.about__block__info}>*/}
-          {/*    <h4>Go live<br />*/}
-          {/*      Q2 2022</h4>*/}
-          {/*    <p>*/}
-          {/* eslint-disable-next-line max-len */}
-          {/*      Basic operations for the limited number of markets and provide the liquidation mechanism with partly automated internal processes*/}
-          {/*    </p>*/}
-          {/*  </div>*/}
-          {/*</div>*/}
-          {/*<div className={s.about__block}>*/}
-          {/*  <img src={About2} alt="Reward"/>*/}
-          {/*  <div className={s.about__block__info}>*/}
-          {/*    <h4>Grantee Near<br />*/}
-          {/*      foundation</h4>*/}
-          {/*    <p>*/}
-          {/*      Grant will help our team launch the project & pass security audit.*/}
-          {/*    </p>*/}
-          {/*  </div>*/}
-          {/*</div>*/}
-          {/*<div className={s.about__block}>*/}
-          {/*  <img src={About3} alt="Reward"/>*/}
-          {/*  <div className={s.about__block__info}>*/}
-          {/*    <h4>Near.Hackbr<br />*/}
-          {/*      winner</h4>*/}
-          {/*    <p>*/}
-          {/*      The Near.Hack-winning solution is bringing*/}
-          {/* eslint-disable-next-line max-len */}
-          {/*      non-custodial borrowing and lending to the NEAR MainNet. Are you ready for liquidity protocol on the most user-friendly blockchain?*/}
-          {/*    </p>*/}
-          {/*  </div>*/}
-          {/*</div>*/}
         </div>
       </div>
     </section>
